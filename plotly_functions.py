5899b751e52d6bee5cc75f980c150a5190c9369b
imnicoo7 <nicolasgutierrez029@gmail.com>
2023-08-31 20:11:43 -0500

    cambios en nueva grafica para el mapa

--- plotly_functions.py
+++ plotly_functions.py
@@ -1,117 +1,94 @@
+# Libraries
 import plotly.express as px
-import pandas as pd
 import folium
-from geopy.geocoders import GoogleV3
 from geopy.exc import GeocoderTimedOut
-from geopy.geocoders import Nominatim
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-def scatter_plot(df, x, y, title):
-    """ Grafico de dispercción """
+class PlotlyFunciones:
 
-    fig = px.scatter(x=df[x], y=df[y], title=title)
+    def scatter_plot(self, df, x, y, title, x_label, y_label):
+        """ Grafico de dispercción """
 
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
+        fig = px.scatter(x=df[x], y=df[y], title=title, labels={'x': x_label, 'y': y_label})
 
-    return fig
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
 
+        return fig
 
-def bar_plot(df, x, y, title):
-    """ Grafico de barras """
+    def bar_plot(self, df, x, y, title):
+        """ Grafico de barras """
 
-    # Crear el gráfico de barras con Plotly
-    fig = px.bar(df, x=df[x], y=df[y], title=title)
+        # Crear el gráfico de barras con Plotly
+        fig = px.bar(df, x=df[x], y=df[y], title=title, labels={x: 'Categoria de Habitaciones', y: 'Número de casas'})
 
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
 
-    return fig
+        return fig
 
+    def mapbox_plot(self, df, x, y):
+        # Crear un mapa centrado en Medellín
+        m = folium.Map(location=[6.253041, -75.564574], zoom_start=12)
 
-import time
-import folium
-
-def mapbox_plot(df, x, y):
-    # Crear un mapa centrado en Medellín
-    m = folium.Map(location=[6.253041, -75.564574], zoom_start=12)
-
-    # Agregar un marcador fijo en las coordenadas 6.253041, -75.564574
-    folium.Marker(
-        location=[6.253041, -75.564574],
-        popup="Ubicación Fija: Medellín",
-        icon=folium.Icon(color="red")  # Puedes personalizar el ícono del marcador
-    ).add_to(m)
-
-    # Iterar a través de las filas del DataFrame
-    for idx, row in df.iterrows():
-        # Obtener las coordenadas (latitud, longitud) desde la columna x
-        latitud, longitud = [float(coord.strip()) for coord in row[x].split(',')]
-
-        # Agregar un marcador al mapa
+        # Agregar un marcador fijo en las coordenadas 6.253041, -75.564574
         folium.Marker(
-            location=(latitud, longitud),
-            popup=f"Coordenadas: {latitud}, {longitud}, Precio: ${row[y]}",
+            location=[6.253041, -75.564574],
+            popup="Ubicación Fija: Medellín",
+            icon=folium.Icon(color="red")  # Puedes personalizar el ícono del marcador
         ).add_to(m)
 
-    return m
+        # Iterar a través de las filas del DataFrame
+        for idx, row in df.iterrows():
+            # Obtener las coordenadas (latitud, longitud) desde la columna x
+            latitud, longitud = [float(coord.strip()) for coord in row[x].split(',')]
 
-def mapbox_plot():
-    m = folium.Map(location=[6.253041, -75.564574], zoom_start=12)
+            # Agregar un marcador al mapa
+            folium.Marker(
+                location=(latitud, longitud),
+                popup=f"Coordenadas: {latitud}, {longitud}, Precio: ${row[y]}",
+            ).add_to(m)
 
-    return m
+        return m
 
+    def mapbox_plot(self):
+        m = folium.Map(location=[6.253041, -75.564574], zoom_start=12)
 
+        return m
 
-def get_coordinates(geolocator, location):
-    try:
-        result = geolocator.geocode(location)
-        if result is not None:
-            return (result.latitude, result.longitude)
-        else:
-            return None
-    except GeocoderTimedOut:
-        return None
+    def box_plot(self, df, x, title):
+        """ Grafico de bigotes """
+        fig = px.box(df, y=df[x], title=title)
 
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
 
-def box_plot(df, x, title):
+        return fig
 
-    """ Grafico de bigotes """
-    fig = px.box(df, y=df[x], title=title)
+    def correlation_matrix(self, df, title):
+        """Funcion para crear una matrix de correlacion y ver los datos"""
 
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
+        fig = px.imshow(df, color_continuous_scale='RdBu', zmin=-1, zmax=1, title=title)
+        # Agregar texto a cada celda con los valores de correlación
+        for i in range(len(df.columns)):
+            for j in range(len(df.columns)):
+                fig.add_annotation(
+                    x=i, y=j,
+                    text=f"{df.iloc[i, j]:.4f}",
+                    showarrow=False,
+                    font=dict(size=20),
+                    bgcolor='black'
+                )
 
-    return fig
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
+        return fig
 
+    def histrogram(self, df, x, title):
+        fig = px.histogram(df, x=x, nbins=20, title=title)
 
-def correlation_matrix(df, title):
+        fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
+        fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
 
-    """Funcion para crear una matrix de correlacion y ver los datos"""
-
-    fig = px.imshow(df, color_continuous_scale='RdBu', zmin=-1, zmax=1, title=title)
-    # Agregar texto a cada celda con los valores de correlación
-    for i in range(len(df.columns)):
-        for j in range(len(df.columns)):
-            fig.add_annotation(
-                x=i, y=j,
-                text=f"{df.iloc[i, j]:.4f}",
-                showarrow=False,
-                font=dict(size=20),
-                bgcolor='black'
-            )
-
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
-    return fig
-
-
-def histograma(df, x, title):
-
-    fig = px.histogram(df, x='Tamaño', nbins=10, title=title)
-
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='white')
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='white')
-
-    return fig
+        return fig
